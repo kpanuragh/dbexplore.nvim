42d890d343a3ff152251de3ba04e2db7819e861a
Anuragh K P <kpanuragh@gmail.com>
2024-06-23 23:38:28 +0530

    Integrate Telescope with database selection functionality
     This commit introduces the integration of the Telescope plugin into the database selection functionality within the dbexplore
     module. The previous implementation of `select_database` function simply executed a shell command to list databases and printe
     the result. Now, the function has been enhanced to utilize Telescope's user interface components, providing a more interactive
     and user-friendly way to select a database.
    
     The changes include requiring necessary Telescope modules, such as actions, finders, pickers, and previewers, and configuring
     them to display the list of databases as selectable entries. Additionally, the commit adds a new table construction loop that
     parses the shell command output into a Lua table, which is then passed to Telescope's picker.
    
     The picker is configured with custom mappings so that when a user selects a database and presses Enter, the `select_table`
     function is called with the chosen database as an argument, allowing for further interaction.
    
     This enhancement not only improves the usability of the database selection process but also leverages the powerful features of
     the Telescope plugin to make the workflow within Neovim more efficient and pleasant for the user.

--- lua/dbexplore/init.lua
+++ lua/dbexplore/init.lua
@@ -4,14 +4,45 @@ local user = 'root'
 local password = 'dev'
 local database = 'mysql'
 
--- Function to display MySQL database selection
-function M.select_database()
-    local handle = io.popen('mariadb -h ' .. host .. ' -u ' .. user .. ' -p' .. password .. ' -e "SHOW DATABASES;"')
-    local result = handle:read("*a")
-    handle:close()
-    print(result)
-end
 
+local telescope = require('telescope')
+    local actions = require('telescope.actions')
+    local finders = require('telescope.finders')
+    local pickers = require('telescope.pickers')
+    local previewers = require('telescope.previewers')
+    local conf = require('telescope.config').values
+-- Function to display MySQL database selection
+    function M.select_database()
+        local handle = io.popen('mariadb -h ' .. host .. ' -u ' .. user .. ' -p' .. password .. ' -e "SHOW DATABASES;"')
+        local result = handle:read("*a")
+        handle:close()
+
+        local database_list = {}
+        for database in result:gmatch('(%S+)') do
+            table.insert(database_list, database)
+        end
+
+        pickers.new({}, {
+            prompt_title = 'Databases',
+            finder = finders.new_table({
+                results = database_list,
+            }),
+            sorter = conf.generic_sorter({}),
+            previewer = previewers.cat.new({}),
+            attach_mappings = function(prompt_bufnr, map)
+                local select_database = function()
+                    local selection = actions.get_selected_entry(prompt_bufnr)
+                    actions.close(prompt_bufnr)
+                    M.select_table(selection.value)
+                end
+
+                map('i', '<CR>', select_database)
+                map('n', '<CR>', select_database)
+
+                return true
+            end,
+        }):find()
+    end
 -- Function to display MySQL table selection
 function M.select_table(database)
     -- Your code here
