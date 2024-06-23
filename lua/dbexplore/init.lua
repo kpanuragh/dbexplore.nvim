2abec076f921be7107660375927ce72fcb8a1af8
Anuragh K P <kpanuragh@gmail.com>
2024-06-23 23:28:59 +0530

    Refactor database selection functionality in dbexplore module
     The database selection functionality in the dbexplore module has been refactored to simplify the codebase and improve
     performance. The previous implementation relied on a custom MySQL connection and cursor handling, which was verbose and involv
     multiple steps for fetching and displaying database information.
    
     The new implementation uses a direct system call to the `mysql` command-line utility, streamlining the process by executing a
     single command that fetches and prints the list of databases. This change eliminates the need for the custom MySQL connection
     functions, reducing the complexity of the code and potential points of failure.
    
     By removing the dependency on the `mysql` Lua module and associated connection management functions, the update also reduces t
     overhead and improves the maintainability of the code. The use of `io.popen` to interact with the `mysql` CLI is a more
     straightforward approach that leverages existing system tools and simplifies the codebase.

--- lua/dbexplore/init.lua
+++ lua/dbexplore/init.lua
@@ -3,71 +3,13 @@ local host ='127.0.0.1'
 local user = 'root'
 local password = 'dev'
 local database = 'mysql'
-local mysql = require("mysql")
 
--- Function to establish a connection to the MySQL database
-function M.connect_database()
-    local conn = mysql.connect{
-        host = host,
-        user = user,
-        password = password,
-        database = database
-    }
-    return conn
-end
-local conn = M.connect_database()
-
--- Function to close the database connection
-function M.close_connection(conn)
-    conn:close()
-end
 -- Function to display MySQL database selection
 function M.select_database()
-    local conn = M.connect_database()
-    local query = "SHOW DATABASES"
-    local cursor = conn:execute(query)
-    local databases = {}
-    for row in cursor:fetch({}, "a") do
-        table.insert(databases, row.Database)
-    end
-    M.close_connection(conn)
-    
-    -- Display databases using telescope.nvim
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local pickers = require("telescope.pickers")
-    local finders = require("telescope.finders")
-    local previewers = require("telescope.previewers")
-    local sorters = require("telescope.sorters")
-    
-    pickers.new({}, {
-        prompt_title = "Databases",
-        finder = finders.new_table({
-            results = databases,
-            entry_maker = function(entry)
-                return {
-                    value = entry,
-                    display = entry,
-                    ordinal = entry,
-                }
-            end,
-        }),
-        sorter = sorters.get_generic_fuzzy_sorter(),
-        attach_mappings = function(prompt_bufnr, map)
-            local select_database = function()
-                local selection = actions.get_selected_entry(prompt_bufnr)
-                if selection then
-                    M.select_table(selection.value)
-                end
-                actions.close(prompt_bufnr)
-            end
-
-            map("i", "<CR>", select_database)
-            map("n", "<CR>", select_database)
-
-            return true
-        end,
-    }):find()
+    local handle = io.popen('mysql -h ' .. host .. ' -u ' .. user .. ' -p' .. password .. ' -e "SHOW DATABASES;"')
+    local result = handle:read("*a")
+    handle:close()
+    print(result)
 end
 
 -- Function to display MySQL table selection
