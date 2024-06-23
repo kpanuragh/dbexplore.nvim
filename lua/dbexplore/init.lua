a391d47d41bbda5efb54a732f15e72582529eba0
Anuragh K P <kpanuragh@gmail.com>
2024-06-23 23:45:54 +0530

    Refactor database and table selection in dbexplore module
     This commit refactors the `select_database` and `select_table` functions in the dbexplore module to improve the user experienc
     and code maintainability. The `select_database` function now stores the selected database name in a global variable
     `M.selected_database`, which simplifies the process of selecting a table by avoiding the need to pass the database name as a
     parameter. Additionally, the `select_table` function has been updated to use the stored database name when executing the query
     to show tables. This change makes the code cleaner and the module's state more predictable, as it relies on the internal state
     of the module rather than function arguments. Overall, these adjustments enhance the module's functionality and make it more
     user-friendly.

--- lua/dbexplore/init.lua
+++ lua/dbexplore/init.lua
@@ -12,46 +12,71 @@ local telescope = require('telescope')
     local previewers = require('telescope.previewers')
     local conf = require('telescope.config').values
 -- Function to display MySQL database selection
-    function M.select_database()
-        local handle = io.popen('mariadb -h ' .. host .. ' -u ' .. user .. ' -p' .. password .. ' -e "SHOW DATABASES;"')
-        local result = handle:read("*a")
-        result = result:gsub("[^\n]*\n", "", 1)
-        handle:close()
+function M.select_database()
+    local handle = io.popen('mariadb -h ' .. host .. ' -u ' .. user .. ' -p' .. password .. ' -e "SHOW DATABASES;"')
+    local result = handle:read("*a")
+    result = result:gsub("[^\n]*\n", "", 1)
+    handle:close()
 
-        local database_list = {}
-        for database in result:gmatch('(%S+)') do
-            table.insert(database_list, database)
-        end
-
-        pickers.new({}, {
-            prompt_title = 'Databases',
-            finder = finders.new_table({
-                results = database_list,
-            }),
-            sorter = conf.generic_sorter({}),
-            previewer = previewers.cat.new({}),
-            attach_mappings = function(prompt_bufnr, map)
-                local select_database = function()
-                    local selection = actions.get_selected_entry(prompt_bufnr)
-                    actions.close(prompt_bufnr)
-                    M.select_table(selection.value)
-                end
-
-                map('i', '<CR>', select_database)
-                map('n', '<CR>', select_database)
-
-                return true
-            end,
-        }):find()
+    local database_list = {}
+    for database in result:gmatch('(%S+)') do
+        table.insert(database_list, database)
     end
+
+    pickers.new({}, {
+        prompt_title = 'Databases',
+        finder = finders.new_table({
+            results = database_list,
+        }),
+        sorter = conf.generic_sorter({}),
+        previewer = previewers.cat.new({}),
+        attach_mappings = function(prompt_bufnr, map)
+            local select_database = function()
+                local selection = actions.get_selected_entry(prompt_bufnr)
+                actions.close(prompt_bufnr)
+                M.selected_database = selection.value -- Store selected database name in global variable
+                M.select_table(selection.value)
+            end
+
+            map('i', '<CR>', select_database)
+            map('n', '<CR>', select_database)
+
+            return true
+        end,
+    }):find()
+end
 -- Function to display MySQL table selection
-function M.select_table(database)
-    -- Your code here
+function M.select_table()
+local database = M.selected_database
+local handle = io.popen('mariadb -h ' .. host .. ' -u ' .. user .. ' -p' .. password .. ' -e "USE ' .. database .. '; SHOW TABLES;"')
+local result = handle:read("*a")
+result = result:gsub("[^\n]*\n", "", 1)
+handle:close()
+local table_list = {}
+for table in result:gmatch('(%S+)') do
+    table.insert(table_list, table)
 end
 
--- Function to display table content in table format
-function M.display_table_content(database, table)
-    -- Your code here
+pickers.new({}, {
+    prompt_title = 'Tables',
+    finder = finders.new_table({
+        results = table_list,
+    }),
+    sorter = conf.generic_sorter({}),
+    previewer = previewers.cat.new({}),
+    attach_mappings = function(prompt_bufnr, map)
+        local select_table = function()
+            local selection = actions.get_selected_entry(prompt_bufnr)
+            actions.close(prompt_bufnr)
+            M.selected_table = selection.value -- Store selected table name in global variable
+            M.display_table_content(M.selected_database, selection.value)
+        end
+        map('i', '<CR>', select_table)
+        map('n', '<CR>', select_table)
+        return true
+    end,
+}):find()
+
 end
 
 return M
